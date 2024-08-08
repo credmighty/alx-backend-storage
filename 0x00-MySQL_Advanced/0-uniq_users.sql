4c47270bfa0e750814d3fece510d89ee586a2639
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-08-09 00:41:15 +0100

    syntax err

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- script for db that creates table users with col id email name
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255)
