cb06836fcf01c2c3c5a10b67636a1d1e4c275cc6
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-08-09 00:38:21 +0100

    create table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- script for db that creates table users with col id email name
+CREATE TABLE IF NOT EXIST users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+	);
